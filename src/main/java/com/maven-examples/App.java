c422923666881e70eb2288235b7148a14112433a
kirankathait <84274231+kirankathait@users.noreply.github.com>
2021-05-31 16:34:15 +0530

    Update App.java

--- src/main/java/com/maven-examples/App.java
+++ src/main/java/com/maven-examples/App.java
@@ -1,11 +1,5 @@
-package com.maven-examples;
-
-/**
- * Hello world!
- *
- */
 public class App {
-    public static void main( String[] args ) {
-        System.out.println("Hello World!");
+    public static main( String[] args ) {
+        System.out.println("Hello World");
     }
 }
